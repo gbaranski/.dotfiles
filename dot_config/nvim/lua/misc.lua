440bc546141dcc0777cb1c5badee175ad6406dd0
gbaranski <root@gbaranski.com>
2021-05-24 12:11:33 +0200

    update

--- dot_config/nvim/lua/misc.lua
+++ dot_config/nvim/lua/misc.lua
@@ -8,49 +8,36 @@ nnoremap('<space><space>', '<C-^>') -- spc-spc to change switch between last buf
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true})
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true})
 
-function _G.init_bufopts()
-  -- Most of these opts come from https://github.com/tjdevries/config_manager
+local nvim_tree = require('nvim-tree.view')
+local fmwinopts = nvim_tree.View.winopts
+local fmbufopts = nvim_tree.View.bufopts
 
-  opt.inccommand      = 'split' -- Nice popup when searching
-  opt.signcolumn      = 'yes:1' -- Enable sign column with width = 1
-  opt.clipboard       = "unnamedplus" -- System clipboard
-  opt.incsearch       = true  -- Makes search act like search in modern browsers
-  opt.showmatch       = false -- Show matching parenthees
-  opt.showmode        = false -- Don't show current mode, I've got statusline for that
-  opt.relativenumber  = true  -- Show line numbers relatively to currrent line
-  opt.number          = true  -- Show current line number
-  opt.ignorecase      = true  -- Ignore case when searching...
-  opt.smartcase       = true  -- ...unless there is capital case
-  opt.hidden          = true  -- Instead of closing hide buffers
-  opt.cursorline      = false -- Highlight current line
-  opt.splitright      = true  -- Prefer splitting to the right
-  opt.splitbelow      = true  -- Prefer splitting to the bottom
-  opt.hlsearch        = true  -- Highlight searches
-  opt.scrolloff       = 10    -- Make it so there are always ten lines below my cursor
-  opt.wrap            = true  -- Wrap lines
-  opt.swapfile        = false -- Disable swap files
-  opt.backup          = false -- Disable backups
-  opt.writebackup     = false -- Disable write backups
+-- Most of these opts come from https://github.com/tjdevries/config_manager
 
-  -- Indentation/Formatting
-  opt.tabstop     = 2    -- tabs are at proper location
-  opt.shiftwidth  = 2    -- indenting is 2 spaces
-  opt.expandtab   = true -- don't use actual tab character (ctrl-v)
-  opt.autoindent  = true -- Copies indent from current line when starting a new line
-  opt.smartindent = true -- Smart auto indenting when starting a new line
-  opt.cindent     = true -- Enables automatic C program indenting
-  opt.breakindent = true -- Break indent
+fmwinopts.relativenumber = true -- Show line numbers relatively to current line on file manager
+fmwinopts.number         = true -- Show current line
 
-  opt.formatoptions = opt.formatoptions
-                      + 'n' -- Recognize numbered lists
-                      + 'j' -- When it makes sense, remove comment leader when joining lines
-                      + 'c' -- Auto wrap comments inserting current comment leader
-                      + 'r' -- Continue coments when using enter
-                      - 'o' -- O and o don't continue comments
-                      - 'a' -- Disable auto formatting
-  
-end
+vim.wo.relativenumber = true -- Show line numbers relatively to currrent line
+vim.wo.signcolumn     = 'yes:1' -- Enable sign colum with width = 1
+vim.wo.number         = true -- Show current line number
+vim.wo.scrolloff    = 10   -- Make it so there are always ten lines below my cursor
+vim.bo.swapfile     = false -- Disable swap files
+vim.bo.tabstop = 4 -- tabs are at proper location
+vim.bo.shiftwidth = 4 -- indenting is 2 spaces
+vim.bo.expandtab = true -- don't use actual tab character (ctrl-v)
+vim.bo.smartindent = true -- Smart auto indenting when starting a new line
+vim.bo.cindent = true-- Enables automatic C program indenting
+vim.bo.formatoptions = 'tcqjn'
+vim.o.hidden          = true  -- Instead of closing hide buffers
 
+vim.o.inccommand      = 'split' -- Nice popup when replacing
+vim.o.clipboard       = "unnamedplus" -- System clipboard
+vim.o.ignorecase      = true  -- Ignore case when searching...
+vim.o.smartcase       = true  -- ...unless there is capital case
+vim.o.splitright      = true  -- Prefer splitting to the right
+vim.o.splitbelow      = true  -- Prefer splitting to the bottom
 
-vim.cmd([[ autocmd FileType * :lua init_bufopts() ]])
-init_bufopts()
+vim.o.incsearch       = true  -- Makes search act like search in modern browsers
+vim.o.showmode        = false -- Don't show current mode, I've got statusline for that
+vim.o.hidden          = true  -- Instead of closing hide buffers
+vim.o.writebackup     = false -- Disable write backups
